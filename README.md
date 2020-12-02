b7c787b50a6560563798760d73492da3804d0067
Valney da Gama Costa <valney.gama@gmail.com>
2020-12-02 08:37:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Markdown WebServer
+
+Servidor Web em NodeJS de resposta única, que lê arquivos **.txt** formatados na linguagem [MarkDown](https://guides.github.com/features/mastering-markdown/), e exibe ordenados em uma página HTML simples.
+
+Consulte os tutoriais de uso da linguagem online:
+ - https://docs.pipz.com/central-de-ajuda/learning-center/guia-basico-de-markdown#open
+ - https://guides.github.com/features/mastering-markdown/
+
---
+++ index.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const fs = require("fs");
+var MarkdownIt = require('markdown-it');
+
+const app = express();
+const md = new MarkdownIt();
+const directoryPath = path.join(__dirname, '.');
+const preDiv = "<div class='html' style='margin:2em auto;max-width:800px;padding:1em;border:solid 1px #ddd;border-radius:3px;'>";
+const posDiv = "</div>";
+
+app.get("/", function(_, res) {
+    fs.readdir(directoryPath, function (err,files) {
+        if (err) return;
+        let responseText = "";
+        
+        for (i = 0; i<files.length; i++) {
+            if (files[i].endsWith(".txt")) {
+                data = fs.readFileSync(files[i], 'utf8');
+                responseText += preDiv + md.render(data) + posDiv;
+            }
+        }
+        res.send(responseText);
+    });
+});
+
+http.createServer(app).listen(80, () => console.log("Servidor rodando local na porta 80"));
+
